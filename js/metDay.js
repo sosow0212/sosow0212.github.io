ad49a2d24d1cf0be94156cd6decc680a812514b0
sosow0212 <sosow0212@naver.com>
2021-09-14 16:46:54 +0900

    pop-page

--- js/metDay.js
+++ js/metDay.js
@@ -1,15 +1,14 @@
 const metDay = document.querySelector(".header-text__p");
 
-function getDays() {
+const getDays = setInterval(() => {
   const today = new Date();
   const dDay = new Date("2021-08-03");
   const gap = today.getTime() - dDay.getTime();
   const result = Math.ceil(gap / (1000 * 60 * 60 * 24));
   metDay.innerText = "만난지 " + result + "일 째";
-}
+}, 1000);
 
-function init() {
-  getDays();
-}
-
-init();
+setTimeout(() => {
+  clearInterval(getDays);
+  console.log('stop');
+},3000)
